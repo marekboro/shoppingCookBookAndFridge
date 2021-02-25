80d0838a1013e997eb97ff238eebb5bcd83118d1
marekboro <marek.borowicz83@gmail.com>
2021-02-25 17:07:28 +0000

    added some data binding and made some dummy recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,7 +1,27 @@
-import { Component } from "@angular/core";
-
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from '../recipe.model';
 @Component({
     selector: 'app-recipe-list',
     templateUrl: './recipe-list.component.html'
 })
-export class RecipeListComponent{}
+export class RecipeListComponent implements OnInit {
+
+    recipes: Recipe[] = [
+        new Recipe(
+            "Fajitas",
+            "Spicy mexican dish that will fill your stomach, satisfy your hunger and burn on the way out...",
+            "https://reciperunner.com/wp-content/uploads/2015/04/SkilletChickenFajitas1.jpg"
+            ),
+        new Recipe(
+            "Shredded chicken in sweet chili sauce",
+            "Chinese food that that is as delicious as it will get you closer to a diabetic coma",
+            "https://reciperunner.com/wp-content/uploads/2015/04/SkilletChickenFajitas1.jpg"
+            )
+      ];
+      constructor() { }
+  
+    ngOnInit(): void {
+    }
+
+}
+
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesComponent implements OnInit {
 
+  
+ 
   constructor() { }
 
   ngOnInit(): void {
